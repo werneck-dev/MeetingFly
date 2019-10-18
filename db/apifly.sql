ae8ee5adc017ba6b30f0f89fdcedf32afb136f97
Raul Werneck <48997136+werneck-dev@users.noreply.github.com>
2019-10-18 00:31:48 -0300

    DUMP

--- db/apifly.sql
+++ db/apifly.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 17-Out-2019 às 23:22
+-- Generation Time: 18-Out-2019 às 00:30
 -- Versão do servidor: 10.1.37-MariaDB
 -- versão do PHP: 7.3.0
 
@@ -47,10 +47,17 @@ CREATE TABLE `meetings` (
 --
 
 INSERT INTO `meetings` (`id`, `id_meeting_room`, `email`, `start_meeting`, `finish_meeting`, `situation`, `description`, `created_at`, `updated_at`) VALUES
-(61, 110, 'rh3@empresa.com', '2019-10-16 06:00:00', '2019-10-16 07:01:00', 'C', 'Reunião para contratacao.', '2019-10-17 18:20:51', '2019-10-17 18:20:51'),
-(62, 105, 'rh3@empresa.com', '2019-10-16 06:00:00', '2019-10-16 07:01:00', 'A', 'Reunião para contratacao.', '2019-10-17 19:55:43', '2019-10-17 19:55:43'),
-(63, 115, 'rh3@empresa.com', '2019-10-16 06:00:00', '2019-10-16 07:01:00', 'A', 'Reunião para contratacao.', '2019-10-17 22:17:57', '2019-10-17 22:17:57'),
-(64, 210, 'rh3@empresa.com', '2019-10-17 06:00:00', '2019-10-16 07:01:00', 'A', 'Reunião para contratacao.', '2019-10-17 22:44:06', '2019-10-17 22:44:06');
+(61, 110, 'rh3@empresa.com', '2019-10-16 06:00:00', '2019-10-16 07:01:00', 'A', 'Reuniao para contratacao.', '2019-10-17 18:20:51', '2019-10-17 18:20:51'),
+(62, 105, 'rh3@empresa.com', '2019-10-17 06:00:00', '2019-10-16 07:01:00', 'A', 'Reuniao para contratacao.', '2019-10-17 19:55:43', '2019-10-17 19:55:43'),
+(63, 115, 'rh3@empresa.com', '2019-10-18 06:00:00', '2019-10-16 07:01:00', 'A', 'Reuniao para contratacao.', '2019-10-17 22:17:57', '2019-10-17 22:17:57'),
+(64, 210, 'rh3@empresa.com', '2019-10-19 06:00:00', '2019-10-16 07:01:00', 'A', 'Reuniao para contratacao.', '2019-10-17 22:44:06', '2019-10-17 22:44:06'),
+(65, 216, 'rh3@empresa.com', '2019-10-20 06:00:00', '2019-10-16 07:01:00', 'A', 'Reuniao para contratacao.', '2019-10-18 02:24:23', '2019-10-18 02:24:23'),
+(66, 217, 'rh3@empresa.com', '2019-10-16 06:00:00', '2019-10-16 07:01:00', 'A', 'Reuniao para contratacao.', '2019-10-18 03:08:41', '2019-10-18 03:08:41'),
+(67, 217, 'rh3@empresa.com', '2019-10-16 07:02:00', '2019-10-16 07:05:00', 'A', 'Reuniao para contratacao.', '2019-10-18 03:09:09', '2019-10-18 03:09:09'),
+(68, 220, 'rh3@empresa.com', '2019-10-19 07:02:00', '2019-10-19 07:05:00', 'A', 'Reuniao para contratacao.', '2019-10-18 03:28:51', '2019-10-18 03:28:51'),
+(69, 220, 'rh3@empresa.com', '2019-10-20 07:02:00', '2019-10-20 07:05:00', 'A', 'Reuniao para contratacao.', '2019-10-18 03:29:00', '2019-10-18 03:29:00'),
+(70, 220, 'rh3@empresa.com', '2019-10-21 07:02:00', '2019-10-21 07:05:00', 'A', 'Reuniao para contratacao.', '2019-10-18 03:29:06', '2019-10-18 03:29:06'),
+(71, 220, 'rh3@empresa.com', '2019-10-22 07:02:00', '2019-10-22 07:05:00', 'A', 'Reuniao para contratacao.', '2019-10-18 03:29:12', '2019-10-18 03:29:12');
 
 -- --------------------------------------------------------
 
@@ -97,7 +104,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `meetings`
 --
 ALTER TABLE `meetings`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- AUTO_INCREMENT for table `migrations`
